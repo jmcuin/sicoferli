758b640100f9bcfc9b4553a949b88a4efc864de1
unknown <jmcuin@gmail.com>
2019-05-20 20:29:50 -0500

    Se agregaron validaciones a panel

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -17,7 +17,7 @@ class PanelController extends Controller
      */
 
     function __construct(){
-        //$this -> middleware(['auth', 'roles:administracion_sitio,direccion_general,direccion_nivel,profesor,administracion,asistencia_administrativa,alumno']);
+        $this -> middleware(['auth', 'roles:administracion_sitio,direccion_general,direccion_nivel,profesor,administracion,asistencia_administrativa,alumno']);
     }
     
     public function index()
