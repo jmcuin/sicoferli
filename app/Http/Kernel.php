76d58c02aa7bc2508631864ac87d3b614898b32f
unknown <jmcuin@gmail.com>
2019-05-30 20:04:57 -0500

    Se quito la ultima coma al middleware roles v3

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\HTTP\Middleware\CheckRoles::class
     ];
 
     /**
@@ -35,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\HTTP\Middleware\CheckRoles::class
+            \App\HTTP\Middleware\CheckRoles::class,
         ],
 
         'api' => [
@@ -58,6 +59,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'roles' => \App\HTTP\Middleware\CheckRoles::class
+        'roles' => \App\HTTP\Middleware\CheckRoles::class,
     ];
 }
